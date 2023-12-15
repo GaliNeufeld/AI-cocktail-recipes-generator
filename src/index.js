73ab52aec1627f11a29baefee0bb1dd4230b7002
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2023-12-14 19:00:32 -0500

    Added basic form handling & added Typewriter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 function generateRecipe(event) {
   event.preventDefault();
-  alert("Here is the recipe");
+ let cocktailNameElement = document.querySelector("#cocktail-name");
+ cocktailNameElement.innerHTML = "sex on the beach";
+ let ingredientsListElement = document.getElementById("ingredients-list");
+ 
+ typewriter = new Typewriter("#ingredients-list", {
+   autoStart: true,
+   cursor: "",
+   delay: 50,
+ });
+
+ typewriter
+   .typeString("<li>1 1/2 oz vodka</li>")
+   .typeString("<li>1/2 oz peach schnapps</li>")
+   .typeString("<li>2 oz cranberry juice</li>")
+   .typeString("<li>2 oz orange juice</li>")
+   .typeString("<li>Orange slice and maraschino cherryfor garnish</li>")
+   .start();
+
+   
+
+typewriter = new Typewriter("#list-of-instructions", {
+  autoStart: true,
+  cursor: "",
+  delay: 50,
+});
+
+typewriter
+  .typeString("<li>Fill a shaker with ice cubes.</li>")
+  .typeString(
+    "<li>Add the vodka, peach schnapps, cranberry juice, and orange juice to the shaker.</li>"
+  )
+  .typeString(
+    "<li>Secure the lid on the shaker and shake well to combine and chill the ingredients.</li>"
+  )
+  .typeString(
+    "<li>Strain the mixture into a highball glass filled with ice.</li>"
+  )
+  .typeString("<li>Garnish with an orange slice and a maraschino cherry.</li>")
+  .start();
+
+
 }
 
 let cocktailRecipeForm = document.querySelector("#cocktail-recipe-form");
-cocktailRecipeForm.addEventListener("sumbit", generateRecipe);
+cocktailRecipeForm.addEventListener("submit", generateRecipe);
