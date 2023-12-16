4f31b0a99ed96a4ecd98b82210e5da84b39cd53c
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2023-12-16 15:22:47 -0500

    Added JS for IA generated recipe
    Made some changes to display one recipe upon download and then fetch and display the user's requested recipe.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-function generateRecipe(event) {
-  event.preventDefault();
- let cocktailNameElement = document.querySelector("#cocktail-name");
- cocktailNameElement.innerHTML = "sex on the beach";
- let ingredientsListElement = document.getElementById("ingredients-list");
- 
- typewriter = new Typewriter("#ingredients-list", {
-   autoStart: true,
-   cursor: "",
-   delay: 50,
- });
+typewriter = new Typewriter("#ingredients-list", {
+  autoStart: true,
+  cursor: "",
+  delay: 50,
+});
 
- typewriter
-   .typeString("<li>1 1/2 oz vodka</li>")
-   .typeString("<li>1/2 oz peach schnapps</li>")
-   .typeString("<li>2 oz cranberry juice</li>")
-   .typeString("<li>2 oz orange juice</li>")
-   .typeString("<li>Orange slice and maraschino cherryfor garnish</li>")
-   .start();
-
-   
+typewriter
+  .typeString("<li>1 1/2 oz vodka</li>")
+  .typeString("<li>1/2 oz peach schnapps</li>")
+  .typeString("<li>2 oz cranberry juice</li>")
+  .typeString("<li>2 oz orange juice</li>")
+  .typeString("<li>Orange slice and maraschino cherryfor garnish</li>")
+  .start();
 
 typewriter = new Typewriter("#list-of-instructions", {
   autoStart: true,
@@ -40,8 +32,35 @@ typewriter
   .typeString("<li>Garnish with an orange slice and a maraschino cherry.</li>")
   .start();
 
-
-}
-
 let cocktailRecipeForm = document.querySelector("#cocktail-recipe-form");
 cocktailRecipeForm.addEventListener("submit", generateRecipe);
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let requestedCocktail = document.querySelector("#requested-cocktail");
+
+  let apiKey = "303b041t9dc7c1ce08f4ao48696a7fa8";
+  let context =
+    "You are an excellent bartender. Your mission is to provide the recipe of a cocktail in basic HTML based on the user instructions. Provide ingredients and instructions only";
+  let prompt = `user instructions: generate a recipe for the cocktail ${requestedCocktail.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayRecipe);
+}
+
+function displayRecipe(response) {
+  // let cocktailNameElement = document.querySelector("#cocktail-name");
+  // cocktailNameElement.innerHTML = "sex on the beach";
+
+  //let ingredientsListElement = document.getElementById("ingredients-list");
+ let sobRecipe = document.querySelector("#sob-recipe");
+sobRecipe.classList.add("hidden");
+
+  new Typewriter("#ai", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
